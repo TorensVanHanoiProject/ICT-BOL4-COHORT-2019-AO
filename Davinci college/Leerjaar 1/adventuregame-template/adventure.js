4fce142990e44f813974d7a2d9a56ce040117d1b
yilmaz <rsd500@outlook.com>
2019-11-27 12:05:35 +0100

    versie 2

--- Davinci college/Leerjaar 1/adventuregame-template/adventure.js
+++ Davinci college/Leerjaar 1/adventuregame-template/adventure.js
@@ -1,51 +1,13 @@
-import java.awt.Color;
-import java.awt.Container;
-import.java.awt.Font;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-
-public class game{
-
-	JFrame window;
-	Container con;
-	JPanel titleNamePanel, startButtonPanel;
-	JLabel titleNameLabel;
-	Font titleFont = new Font("Times New Roman, Font.PLAIN, 90");
-	public static voic man (string[] args){
-
-		new game();
-	}
-
-	public game(){
-
-		window = new JFrame();
-		window.setSize(800,600);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.getContentPane().setBackground(Color.black);
-		window.setLayout(null);
-		window.setVisible(true);
-		con = window.getContentPane();
-
-		titleNamePanel = new JPanel();
-		titleNamePanel.setBounds(100,100,600,150);
-		titleNamePanel.setBackground(Color.black);
-		titleNameLabel = new JLabel("ADVENTURE");
-		titleNameLabel.setForeGround(Color.white);
-		titleNameLabel.setFont(titleFont);
-
-		startButtonPanel = new JPanel();
-		startButtonPanel.setBounds(300,400,200,100);
-		startButtonPanel.setBackground(Color.blue);
-
-		titleNamePanel.add(titleNameLabel);
-		con.add(titleNamePanel);
-		con.add(startButtonPanel);
-
-
-
-	}
-
+function escape(){
+	document.getElementById("demo").innerHTML = "je vader!";
 }
+
+var description = document.getElementById("description");
+var image = document.getElementById("image")
+var button1 = document.getElementById("button1")
+var button2 = document.getElementById("button2")
+var button3 = document.getElementById("button3")
+var inventoryItem = document.getElementById("inventoryItem")
+
+
